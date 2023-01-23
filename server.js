984c50ca1cf843ebd73ac7f0165a119a78df35dc
jirapa <jirapa@hot-now.com>
2023-01-23 21:06:52 +0700

    weekday function

--- server.js
+++ server.js
@@ -6,42 +6,42 @@ var server = app.listen(8081, function () {
   var port = server.address().port;
 });
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+// app.get("/", function (req, res) {
+//   res.send("Hello World");
+// });
 
 app.get("/api/weekday/:inputDate", (req, res, next) => {
-  //   if (!req.params.inputDate) {
-  //     res.send("Please input date.");
-  //   }
   var inputDate = req.params.inputDate;
-  console.log("input date: " + inputDate);
-  console.log(typeof inputDate);
+  console.log(inputDate.length);
+  if (inputDate.length < 10) {
+    res.json({ error: "Input date format must be 'YYYY-MM-DD'." });
+  }
+
   let year = inputDate.substring(0, 4);
+
   if (Number(year) < 1900) {
-    res.send("Range of year is from 1900 afterward.");
+    res.json({ error: "Range of year is from 1900 afterward." });
   }
   var date = new Date(inputDate);
-  console.log("new date: " + date);
-  console.log(date instanceof Date && !isNaN(date));
-  console.log(date instanceof Date);
-  console.log("check value : " + !isNaN(date));
+  //   console.log("new date: " + date);
+  //   console.log(date instanceof Date);
+  //   console.log("check value : " + !isNaN(date));
 
-  let day = date.getDay();
+  if (date instanceof Date && !isNaN(date)) {
+    let day = date.getDay();
 
-  console.log(day);
-  const dayOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+    const dayOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-  //   res.json({
-  //     "day of week": "xxx",
-  //   });
-  res.json({ dayOfWeek: dayOfWeek[day] });
+    res.json({ dayOfWeek: dayOfWeek[day] });
+  } else {
+    res.json({ error: "Input date is invalid." });
+  }
 });
